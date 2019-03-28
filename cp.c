1b969eea5d90b1481c491df1c2a6b96754b40791
a-johanes <antoniusjohanes99@gmail.com>
2019-03-28 23:49:02 +0700

    can cp directory

--- cp.c
+++ cp.c
@@ -1,13 +1,15 @@
-#define INSUFFICIENT_SECTORS -4
-#define INSUFFICIENT_ENTRIES -3
-#define ALREADY_EXISTS -2
-#define NOT_FOUND -1
-#define MAX_PATHNAME 512
-#define SECTOR_SIZE 512
+#include "definition.h"
+// #define INSUFFICIENT_SECTORS -4
+// #define INSUFFICIENT_ENTRIES -3
+// #define ALREADY_EXISTS -2
+// #define NOT_FOUND -1
+// #define MAX_PATHNAME 512
+// #define SECTOR_SIZE 512
 
 void intToChar(int angka, char* hasil);
 int mod(int a, int b);
 int div(int a, int b);
+void copy(char* from, char* to,char currDir,int* success);
 
 int main(){
     char argv[128][128];
@@ -32,25 +34,26 @@ int main(){
     interrupt(0x21,0x21,&currDir,0,0);
 
     interrupt(0x21,0x22,&argc,0,0); // ambil argc
-    // intToChar(argc,temp);
-    // interrupt(0x21,0x00,temp,0,0);
+    intToChar(argc,temp);
+    interrupt(0x21,0x00,temp,0,0);
     for(i=0;i<argc,i<3;++i){ // copy semua isi argv ke temp
         interrupt(0x21,0x23,i,argv[i],0);
     }
     if(argc==3){
-        interrupt(0x21,currDir<<8|0x12,argv[1],&result,0);
-        if(result==NOT_FOUND){
-            interrupt(0x21,0x00,"Hehe ga ketemu! lol",0,0);
-        }else if(result==0){ //kalau file
-            interrupt(0x21,currDir<<8|0x13,argv[1],&length,0);
+        copy(argv[1],argv[2],currDir,&result);
+        // interrupt(0x21,currDir<<8|0x12,argv[1],&result,0);
+        // if(result==NOT_FOUND){
+        //     interrupt(0x21,0x00,"Hehe ga ketemu! lol",0,0);
+        // }else if(result==0){ //kalau file
+        //     interrupt(0x21,currDir<<8|0x13,argv[1],&length,0);
 
-            nSector = length;
-            interrupt(0x21,currDir<<8|0x04,tempBuffer,argv[1],&readResult);
+        //     nSector = length;
+        //     interrupt(0x21,currDir<<8|0x04,tempBuffer,argv[1],&readResult);
 
-            interrupt(0x21,currDir<<8|0x05,tempBuffer,argv[2],&nSector,0);
-        }else if(result==1){ //kalau direktori
+        //     interrupt(0x21,currDir<<8|0x05,tempBuffer,argv[2],&nSector,0);
+        // }else if(result==1){ //kalau direktori
 
-        }
+        // }
     }else{
         interrupt(0x21,0x00,"hey kebanyakan boi",0,0);
     }
@@ -106,4 +109,101 @@ int div(int a, int b) {
         q = q+1;
     }
     return q-1;
+}
+
+void copy(char* from, char* to,char currDir,int* success){
+    int i;
+    int result;
+    int nSector;
+    int dirIdx;
+    int foundIdx;
+    char tempBuffer[16*512];
+    char directories[SECTOR_SIZE];
+    char files[SECTOR_SIZE];
+    char dirName[MAX_DIRECTORYNAME];
+    char filename[MAX_FILENAME];
+    char tempPathFrom[MAX_PATHNAME];
+    char tempPathTo[MAX_PATHNAME];
+
+    interrupt(0x21,0x2,directories,DIRS_SECTOR,0); // readSector directori
+    interrupt(0x21,0x2,files,FILES_SECTOR,0); // readSector file
+    
+    interrupt(0x21,0x00,from,0,0);
+
+    interrupt(0x21,currDir<<8|0x12,from,&result,0);//panggil isDirectory
+    if(result==NOT_FOUND){//kalau bukan file ataupun direktori
+        interrupt(0x21,0x00,"Hehe ga ketemu! lol",0,0);
+        *success=NOT_FOUND;
+    }else if(result==0){ //kalau file
+        interrupt(0x21,currDir<<8|0x13,from,&nSector,0);//getFileSize
+        interrupt(0x21,currDir<<8|0x04,tempBuffer,from,&result);//readFile
+        //mungkin hang karena overuse result
+        if(result==0){
+            interrupt(0x21,currDir<<8|0x05,tempBuffer,to,&nSector);//writeFile
+            *success=nSector;//set success
+        }else{
+            interrupt(0x21,0x00,"gagal read",0,0);
+        }
+    }else if(result==1){ //kalau direktori
+        interrupt(0x21,currDir<<8|0x08,to,&result,0);//makeDirectory
+        //mungkin hang karena overuse result
+        if(result==0 || result==ALREADY_EXISTS){//berhasil makeDirectory
+            interrupt(0x21,currDir<<8|0x10,from,dirName,&dirIdx);//pathparser
+            interrupt(0x21,dirIdx<<8|0x11,dirName,directories,&foundIdx);//finder
+            if(foundIdx!=NOT_FOUND){
+                for(i=0;i<MAX_DIRECTORY;i++){
+                    if(directories[i*DIR_ENTRY_LENGTH]==foundIdx && directories[i*DIR_ENTRY_LENGTH+1]!='\0'){
+                        stringCopy(directories,dirName,i*DIR_ENTRY_LENGTH+1,MAX_DIRECTORYNAME);
+                        //mungkin error di bawah ini
+                        stringCopy(from,tempPathFrom,0,MAX_PATHNAME);
+                        stringConcat(tempPathFrom,"/",tempPathFrom);
+                        stringConcat(tempPathFrom,dirName,tempPathFrom);
+                        //di sini ada print
+                        interrupt(0x21,0x00,tempPathFrom,0,0);
+
+                        stringCopy(to,tempPathTo,0,MAX_PATHNAME);
+                        stringConcat(tempPathTo,"/",tempPathTo);
+                        stringConcat(tempPathTo,dirName,tempPathTo);
+                        //di sini ada print
+                        interrupt(0x21,0x00,tempPathTo,0,0);
+                        copy(tempPathFrom,tempPathTo,currDir,&result);
+                        if(result!=0){
+                            *success=result;
+                            return;
+                        }
+                    }
+                }
+                for(i=0;i<MAX_FILES;i++){
+                    if(files[i*DIR_ENTRY_LENGTH]==foundIdx && files[i*DIR_ENTRY_LENGTH+1]!='\0'){
+                        stringCopy(files,filename,i*DIR_ENTRY_LENGTH+1,MAX_FILENAME);
+                        //mungkin error di bawah ini
+                        stringCopy(from,tempPathFrom,0,MAX_PATHNAME);
+                        stringConcat(tempPathFrom,"/",tempPathFrom);
+                        stringConcat(tempPathFrom,filename,tempPathFrom);
+                        //di sini ada print
+                        interrupt(0x21,0x00,tempPathFrom,0,0);
+
+                        stringCopy(to,tempPathTo,0,MAX_PATHNAME);
+                        stringConcat(tempPathTo,"/",tempPathTo);
+                        stringConcat(tempPathTo,filename,tempPathTo);
+                        //di sini ada print
+                        interrupt(0x21,0x00,tempPathTo,0,0);
+                        copy(tempPathFrom,tempPathTo,currDir,&result);
+                        if(result!=0){
+                            *success=result;
+                            return;
+                        }
+                    }
+                }
+                *success=0;
+            }else{
+                interrupt(0x21,0x00,"gagal foundidx",0,0);
+                *success=NOT_FOUND;
+            }
+        }else{
+            *success=result;
+        } 
+    }
+
+
 }
