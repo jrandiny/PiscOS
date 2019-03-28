247d5f0bfc79615fa308520856b03316506970ba
a-johanes <antoniusjohanes99@gmail.com>
2019-03-28 23:46:44 +0700

    add error message to mkdir

--- mkdir.c
+++ mkdir.c
@@ -4,10 +4,13 @@ int main(){
     char temp[128];
     char print[2];
     int curDir;
+    int suc;
 
-    interrupt(0x21,0x23,0,temp,0); // ambil argumen pertama
+    interrupt(0x21,0x23,1,temp,0); // ambil argumen pertama
     interrupt(0x21,0x21,&curDir,0,0); // ambil directori sekarang
-    interrupt(0x21,curDir<<8 | 0x8,temp,0,0); // panggil makeDirectory
+    interrupt(0x21,curDir<<8 | 0x8,temp,&suc,0); // panggil makeDirectory
+    if(suc!=0) 
+        interrupt(0x21,0x00,"mkdir: cannot create directory '': No such file or directory",0,0);
     interrupt(0x21,0x7,0,0,0); // terminateProgram
     return 0;
 }
